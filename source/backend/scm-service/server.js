0791458014dabc37a3aac5b01a3c90fe792f96a7
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-02-28 14:10:53 -0300

    Enable cors

--- source/backend/scm-service/server.js
+++ source/backend/scm-service/server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
+
 const github = require("./api/github");
 
 const app = express();
 
 const apiPrefix = "/api/scm";
 
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Enable cors
+app.use(cors());
 
 app.use(`${apiPrefix}/github`, github);
 app.get(`${apiPrefix}/health`, (req, res) => res.json({ msg: "Health OK" }));
